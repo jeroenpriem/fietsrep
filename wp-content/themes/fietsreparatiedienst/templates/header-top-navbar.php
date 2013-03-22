4ebc7e696f9f1a3078756e4eb356350c07a9883d
Jeroen Priem <jeroen@gouwestadmm.nl>
2013-03-22 11:18:36 +0100

    fietsreparatiedienst.nl

---
+++ wp-content/themes/fietsreparatiedienst/templates/header-top-navbar.php
@@ -0,0 +1,49 @@
+<header>
+    <div class="header">
+    </div>
+    <div class="container">
+      <div class="row-fluid clearfix">
+          <div class="span4">
+          </div>
+          <div class="span4">
+            <a class="brand" href="<?php echo home_url(); ?>/">
+            <img src="http://www.gouwestad.com/fietsreparatiedienst/assets/img/logo.png" alt="logo"/>
+            </a>
+          </div>
+          <div class="span4">
+          </div>
+      </div>
+      <div class="row-fluid clearfix">
+        <div class="navbar">
+          <div class="navbar-inner">
+            <div class="container">
+              <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </a>
+              <nav class="nav-collapse" role="navigation">
+                <?php
+                  if (has_nav_menu('primary_navigation')) :
+                    wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav center'));
+                  endif;
+                ?>
+              </nav>
+            </div>
+          </div>
+      </div>       
+    
+    </div>         
+        <div class="row-fluid clearfix">
+          <div class="span3">
+          </div>
+          <div class="span6">
+            <h1>Fiets kapot?</h1>
+              <p>Zoek hier de dichtstbijzijnde fietsenmaker</p>
+              <a href="#" class="btn btn-large btn-primary btn-block" id="zoekform-link"> Zoeken</a>
+          <div class="span3">
+          </div>
+          </div>
+        </div>
+    </div>
+  </header>
