4655f93ec9e13d1a678517c317d5e1dbd66614d9
Lex <lex@gouwestadmm.nl>
2013-03-22 15:56:48 +0100

    Update homepage

--- wp-content/themes/fietsreparatiedienst/home.php
+++ wp-content/themes/fietsreparatiedienst/home.php
@@ -6,15 +6,25 @@ Template Name: Home
 
 <section class="fullscreen">
   <div class="container">
-    <div class="row-fluid">
-      <div class="span12 center">
-      <img src="http://www.gouwestad.com/fietsreparatiedienst/assets/img/logo.png" class="big-logo">
-      <form class="form-inline">
+    <div class="row">
+      <div class="span12 center big-logo">
+        <img src="http://www.gouwestad.com/fietsreparatiedienst/assets/img/logo.png">
+      </div>
+    </div>
+    <div class="row">
+      <div class="span1"></div>
+      <div class="span6">
         <input type="text" class="input-large input-block" placeholder="Email">
-        <button type="submit" class="btn btn-large">Blijf op de hoogte</button>
-      </form>
-      <p><i class="icon-play-circle"></i> alvast weten wat wij voor je gaan doen</p>
+      </div>
+      <div class="span4">
+        <button type="submit" class="btn btn-large btn-block btn-primary">Blijf op de hoogte</button>
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="span12 center">
+        <h3><i class="glyphicon glyphicon-play-circle"></i> alvast weten wat wij voor je gaan doen</h3>
       </div>
     </div>
   </div>
-</header>
+</section>
--- wp-content/themes/fietsreparatiedienst/templates/header-top-navbar.php
+++ wp-content/themes/fietsreparatiedienst/templates/header-top-navbar.php
@@ -1,30 +1,19 @@
-<header>
-    <div class="header">
+<div class="navbar navbar-fixed-top">  
+  <div class="container">
+    <button data-target=".nav-collapse" data-toggle="collapse" class="navbar-toggle" type="button">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a href="<?php echo home_url(); ?>/" class="navbar-brand"> <img src="http://www.gouwestad.com/fietsreparatiedienst/assets/img/logo.png" alt="logo"/></a>
+    <div class="nav-collapse collapse">
+      <?php
+      if (has_nav_menu('primary_navigation')) :
+        wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav center'));
+      endif;
+      ?>
     </div>
-    <div class="container">
-      <div class="row-fluid clearfix">
-        <div class="navbar">
-          <div class="navbar-inner">
-            <div class="container">
-              <a class="brand" href="<?php echo home_url(); ?>/">
-            <img src="http://www.gouwestad.com/fietsreparatiedienst/assets/img/logo.png" alt="logo"/>
-            </a>
-              <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </a>
-              <nav class="nav-collapse" role="navigation">
-                <?php
-                  if (has_nav_menu('primary_navigation')) :
-                    wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav center'));
-                  endif;
-                ?>
-              </nav>
-            </div>
-          </div>
-      </div>       
-    
-    </div> 
-  </div>        
-</header>
+  </div>
+</div>
+     
+
