00c089862d526a43f1a336940c77f45f0fbcc153
Lex <lex@gouwestadmm.nl>
2013-04-08 16:53:55 +0200

    Revert "wp-config activate multisite"
    This reverts commit 557ed0df53521bfd8e78f7f4188f58c876fcbdde.

--- wp-config.php
+++ wp-config.php
@@ -91,6 +91,3 @@ require_once(ABSPATH . 'wp-settings.php');
 
 define('WP_HOME','http://192.168.1.184/~lexbijl/fietsrep/');
 define('WP_SITEURL','http://192.168.1.184/~lexbijl/fietsrep/');
-
-/* Multisite */
-define('WP_ALLOW_MULTISITE', true);
