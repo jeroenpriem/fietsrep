cb4ffedf9b2276dca395ee4ac2b745337e11cc7e
Lex <lex@gouwestadmm.nl>
2013-04-08 16:53:55 +0200

    Revert "Revert "update""
    This reverts commit bb674cd59e59d19a975ff3e2e3a4165390279254.

--- wp-content/themes/fietsreparatiedienst/home.php
+++ wp-content/themes/fietsreparatiedienst/home.php
@@ -5,25 +5,51 @@ Template Name: Home
 ?>
 
 <section class="fullscreen">
+  <div class="overlay"></div>
   <div class="container">
     <div class="row">
       <div class="span12 center big-logo">
-        <img src="http://www.gouwestad.com/fietsreparatiedienst/assets/img/logo.png">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png">
+      </div>
+    </div> 
+    <div class="subscribe">
+      <div class="row">
+        <div class="span8">
+          <input type="text" class="input-large input-block" placeholder="Email">
+        </div>
+        <div class="span4">
+          <button type="submit" class="btn btn-large btn-block btn-primary">Blijf op de hoogte</button>
+        </div>
+      </div>
+      <hr>
+      <div class="row">
+        <div class="span12 center">
+          <h3><a href="#story"><i class="glyphicon glyphicon-play-circle"></i> vind direct een fietsenmaker in de buurt</a></h3>
+        </div>
       </div>
     </div>
-    <div class="row">
-      <div class="span1"></div>
-      <div class="span6">
-        <input type="text" class="input-large input-block" placeholder="Email">
-      </div>
-      <div class="span4">
-        <button type="submit" class="btn btn-large btn-block btn-primary">Blijf op de hoogte</button>
+  </div>
+  <div class="scrollindicator">
+    <a href="#story"><i class="glyphicon glyphicon-arrow-down"></i></a>
+  </div>
+</section>
+
+<section class="story" id="story">
+  <div class="container">
+     <div class="row">
+      <div class="span12">
+        <h3 class="center">Met onze Fietsreparatiedienst vind je hier binnenkort een superhandige locator om de dichtsbijzijnde fietsenmakers te vinden. Wil jij op de hoogte 
+          blijven? Schrijf je dan in en maak gelijk kans op de gratis voorjaarscheck van je fiets bij een fietsenmaker bij jou in de buurt!</h3>
       </div>
     </div>
     <hr>
     <div class="row">
-      <div class="span12 center">
-        <h3><i class="glyphicon glyphicon-play-circle"></i> alvast weten wat wij voor je gaan doen</h3>
+      <div class="span6">
+        John heeft een probleem. Hij is onderweg naar zijn werk toe en zijn band klapt. Hij heeft niet veel tijd en moet toch door naar kantoor. John heeft geen gereedschap en geen plaksetje bij zich.
+      </div>
+    </div>
+    <div class="row">
+      <div class="span6">
       </div>
     </div>
   </div>
